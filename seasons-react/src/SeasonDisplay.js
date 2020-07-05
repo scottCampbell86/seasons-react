8c287d390327834cac3f4405a9f659d368ff2b5a
Scott Campbell <stc9486@gmail.com>
2020-07-05 18:49:44 -0400

    sd

---
+++ seasons-react/src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return (
+    <div>Hi, I'm Season Display</div>
+  )
+}
+
+export default SeasonDisplay;
--- seasons-react/src/index.js
+++ seasons-react/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 const App = () => {
-  return <div>Hi, i'm App</div>
+  return (
+    <>
+      <div>Hi, i'm App</div>
+      <SeasonDisplay />
+    </>
+  )
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'))
