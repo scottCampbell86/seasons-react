d4914a3d0c3aed4f86850eee72924dce3e40cef2
Scott Campbell <stc9486@gmail.com>
2020-07-06 15:45:07 -0400

    refact

--- seasons-react/src/index.js
+++ seasons-react/src/index.js
@@ -23,13 +23,10 @@ class App extends React.Component {
 
   render () {
 
-    return (
-      <div>
-        Latitude: { this.state.lat }
-        <br />
-        Error: { this.state.errorMessage }
-      </div>
-    )
+
+    if (this.state.errorMessage && !this.state.lat) return <div>Error: { this.state.errorMessage }</div>
+    if (!this.state.errorMessage && this.state.lat) return <div>Latitude: { this.state.lat }</div> 
+    return <div>Loading!</div>
   }
 }
 ReactDOM.render(<App />, document.querySelector('#root'))
