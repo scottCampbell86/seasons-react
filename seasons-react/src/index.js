31023b566332de0ab94685a58ad782b39abcf27e
Scott Campbell <stc9486@gmail.com>
2020-07-06 16:39:01 -0400

    didM

--- seasons-react/src/index.js
+++ seasons-react/src/index.js
@@ -10,19 +10,16 @@ class App extends React.Component {
         lat: null, 
         errorMessage: '' 
     }
+  }
 
+  componentDidMount = () => {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render () {
-
 
     if (this.state.errorMessage && !this.state.lat) return <div>Error: { this.state.errorMessage }</div>
     if (!this.state.errorMessage && this.state.lat) return <div>Latitude: { this.state.lat }</div> 
