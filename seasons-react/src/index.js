9908c95eb75160f4b7a075c617b4fffe0aae67f2
Scott Campbell <stc9486@gmail.com>
2020-07-06 15:10:42 -0400

    construct

--- seasons-react/src/index.js
+++ seasons-react/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
+//import SeasonDisplay from './SeasonDisplay'
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return (
-    <>
-      <div>Hi, i'm App</div>
-      <SeasonDisplay />
-    </>
-  )
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { lat: null }
+  }
+  
+  render () {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>
+  }
+}
 ReactDOM.render(<App />, document.querySelector('#root'))
