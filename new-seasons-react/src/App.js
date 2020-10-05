25cc7abb0c8d66d170a4a5f422c8a3719e0592ab
Scott Campbell <stc9486@gmail.com>
2020-10-05 19:33:34 -0400

    ok

--- new-seasons-react/src/App.js
+++ new-seasons-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import SeasonsDisplay from './SeasonsDisplay';
+import Spinner from './Spinner'
 
 class App extends React.Component {
-  constructor (props) {
-    super(props);
+  
+  state = { lat: null, errorMessage: '' };
+  
 
-    this.state = { lat: null, errorMessage: '' };
-    
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
@@ -16,14 +18,15 @@ class App extends React.Component {
       }
     );
   }
+
   render () {
     if (this.state.errorMessage && !this.state.lat) 
     return <div>Error: {this.state.errorMessage}</div>
 
     if (!this.state.errorMessage && this.state.lat)
-    return <div> Latitude: {this.state.lat}</div>
+    return <div><SeasonsDisplay lat={this.state.lat} /></div>
     
-    return <div>Loading!</div>
+    return <div><Spinner /></div>
   }
 }
 
--- new-seasons-react/src/SeasonsDisplay.js
+++ new-seasons-react/src/SeasonsDisplay.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hi, I'm app</div>
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) return lat > 0 ? 'summer' : 'winter';
+  else return lat > 0 ? 'winter' : 'summer'
+};
+
+const config = {
+  summer: {
+    text: `it's summer`,
+    icon: `some icon`
+  },
+  winter: {
+    text: `it's winter`,
+    icon: `some icon`
+  }
+}
+
+const SeasonsDisplay = (props) => {
+  
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, icon} = config[season]
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
 }
 
 export default SeasonsDisplay;
